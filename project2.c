16f28fb423e2998416ce34c14aec3e35052b3a13
Andrew Massey <amassey107@gmail.com>
2018-10-23 01:16:01 -0500

    Update project2.c

--- project2.c
+++ project2.c
@@ -53,7 +53,7 @@ char *trsh_LINEINPUT(int *argc) {
     char *cwdBuf;
     cwdBuf = calloc(PATH_MAX, sizeof(char));
     cwdBuf = getcwd(cwdBuf, PATH_MAX);
-    if(argc < 2)
+    if(*argc < 2)
     {
         printf("%s==>", cwdBuf);
     }
@@ -84,7 +84,7 @@ char *trsh_LINEINPUT(int *argc) {
         }
     }
     lineBuffer[length] = '\0';
-    if(argc > 1)
+    if(*argc > 1)
     {
         printf("%s==>%s\n", cwdBuf, lineBuffer);
     }
